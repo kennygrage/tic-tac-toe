0f57e52e0e34434e46df31a2d8720e2088e62b67
Kenny <student@epicodus.com>
2015-09-16 14:52:23 -0700

    two player game functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,60 @@
-board = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]; //0 = empty space, 1 = "X", 2 = "O"
+board = [[9, 9, 9], [9, 9, 9], [9, 9, 9]]; //9 = empty space, 1 = "X", 2 = "O"
+var player_one_color = "#d54";
+var player_two_color = "#5a4";
+var player_number;
 
+function startGame() {
+  //start with player one
+  player_number = 1;
+  $("#player_turn").css("color", player_one_color);
+  $("#player_turn").text("Player 1's Turn");
+}
+
+function markBox(clicked) {
+  var row = clicked.substring(1,2);
+  var column = clicked.substring(3,4);
+  var markSquareResult = markSquare(row, column, player_number);
+  if (markSquareResult == "Space already taken" && player_number < 9) {
+    $("#output").text(markSquareResult);
+  }
+  else if (markSquareResult == "Player 1 wins!!") {
+    $("#" + clicked).css("color", player_one_color).text("X");
+    player_number = 9; //make sure the game can't be played anymore
+    $("#player_turn").css("color", player_one_color);
+    $("#player_turn").text("Game Over");
+    $("#output").text(markSquareResult);
+  }
+  else if (markSquareResult == "Player 2 wins!!") {
+    $("#" + clicked).css("color", player_two_color).text("O");
+    player_number = 9; //make sure the game can't be played anymore
+    $("#player_turn").css("color", player_two_color);
+    $("#player_turn").text("Game Over");
+    $("#output").text(markSquareResult);
+  }
+  else if (player_number == 1) {
+    $("#" + clicked).css("color", player_one_color).text("X");
+    player_number = 2;
+    $("#player_turn").css("color", player_two_color);
+    $("#player_turn").text("Player 2's Turn");
+    $("#output").text("");  //empty output incase an error message was there
+  }
+  else if (player_number == 2) {
+    $("#" + clicked).css("color", player_two_color).text("O");
+    player_number = 1;
+    $("#player_turn").css("color", player_one_color);
+    $("#player_turn").text("Player 1's Turn");
+    $("#output").text("");  //empty output incase an error message was there
+  }
+}
 
 function markSquare(row, column, player_number) {
-  if (board[row][column] != 0) {
+  if (board[row][column] != 9) {
     return "Space already taken";
   }
   else {
     board[row][column] = player_number;
     if (win()) {
-      return "Player " + player_number + " wins";
+      return "Player " + player_number + " wins!!";
     }
     else {
       return board;
@@ -17,14 +63,14 @@ function markSquare(row, column, player_number) {
 }
 
 function win() {
-  if ((board[0][0] == board[0][1] && board[0][0] == board[0][2] && board[0][0] > 0) || //top row
-      (board[1][0] == board[1][1] && board[1][0] == board[1][2] && board[1][0] > 0) || //middle row
-      (board[2][0] == board[2][1] && board[2][0] == board[2][2] && board[2][0] > 0) || //bottom row
-      (board[0][0] == board[1][0] && board[0][0] == board[2][0] && board[0][0] > 0) || //left column
-      (board[0][1] == board[1][1] && board[0][1] == board[2][1] && board[0][1] > 0) || //middle column
-      (board[0][2] == board[1][2] && board[0][2] == board[2][2] && board[0][2] > 0) || //right column
-      (board[0][0] == board[1][1] && board[0][0] == board[2][2] && board[0][0] > 0) || //from upper left to lower right diagonal
-      (board[0][2] == board[1][1] && board[0][2] == board[2][0] && board[0][2] > 0) )  //from upper right to lower left diagonal
+  if ((board[0][0] == board[0][1] && board[0][0] == board[0][2] && board[0][0] < 9) || //top row
+      (board[1][0] == board[1][1] && board[1][0] == board[1][2] && board[1][0] < 9) || //middle row
+      (board[2][0] == board[2][1] && board[2][0] == board[2][2] && board[2][0] < 9) || //bottom row
+      (board[0][0] == board[1][0] && board[0][0] == board[2][0] && board[0][0] < 9) || //left column
+      (board[0][1] == board[1][1] && board[0][1] == board[2][1] && board[0][1] < 9) || //middle column
+      (board[0][2] == board[1][2] && board[0][2] == board[2][2] && board[0][2] < 9) || //right column
+      (board[0][0] == board[1][1] && board[0][0] == board[2][2] && board[0][0] < 9) || //from upper left to lower right diagonal
+      (board[0][2] == board[1][1] && board[0][2] == board[2][0] && board[0][2] < 9) )  //from upper right to lower left diagonal
       {
     return true;
   }
@@ -35,10 +81,12 @@ function win() {
 
 
 $(document).ready(function() {
-
+  startGame();
   $(document.body).click(function(evt) {
     var clicked = evt.target.id;
-    $("#" + clicked).text("O");
+    if (clicked.match(/_\d_\d/)) { //don't go into markBox unless one of the 9 boxes are clicked
+      markBox(clicked);
+    }
   }); //end document.body
 }); //end document.ready
 
--- spec/specs.js
+++ spec/specs.js
@@ -1,7 +1,7 @@
 describe('markSquare', function() {
   describe('New game: square can\'t be marked twice', function() {
     it("Player 1 marks the middle square", function() {
-      expect(markSquare(1, 1, 1)).to.eql([[0, 0, 0], [0, 1, 0], [0, 0, 0]]);
+      expect(markSquare(1, 1, 1)).to.eql([[9, 9, 9], [9, 1, 9], [9, 9, 9]]);
     });
 
     it("Player 2 tries to mark the middle square but it is taken", function() {
@@ -12,139 +12,139 @@ describe('markSquare', function() {
 
   describe('New game: Player 1 wins in top row', function() {
     before(function() {
-      board = [[0, 0, 0], [0, 0, 0], [0, 0, 0]];
+      board = [[9, 9, 9], [9, 9, 9], [9, 9, 9]];
     });
 
     it("Player 1 marks the top left square", function() {
-      expect(markSquare(0, 0, 1)).to.eql([[1, 0, 0], [0, 0, 0], [0, 0, 0]]);
+      expect(markSquare(0, 0, 1)).to.eql([[1, 9, 9], [9, 9, 9], [9, 9, 9]]);
     });
 
     it("Player 2 marks the middle square", function() {
-      expect(markSquare(1, 1, 2)).to.eql([[1, 0, 0], [0, 2, 0], [0, 0, 0]]);
+      expect(markSquare(1, 1, 2)).to.eql([[1, 9, 9], [9, 2, 9], [9, 9, 9]]);
     });
 
     it("Player 1 marks the upper right square", function() {
-      expect(markSquare(0, 2, 1)).to.eql([[1, 0, 1], [0, 2, 0], [0, 0, 0]]);
+      expect(markSquare(0, 2, 1)).to.eql([[1, 9, 1], [9, 2, 9], [9, 9, 9]]);
     });
 
     it("Player 2 marks the middle right square", function() {
-      expect(markSquare(1, 2, 2)).to.eql([[1, 0, 1], [0, 2, 2], [0, 0, 0]]);
+      expect(markSquare(1, 2, 2)).to.eql([[1, 9, 1], [9, 2, 2], [9, 9, 9]]);
     });
 
     it("Player 1 marks the upper middle square and wins", function() {
-      expect(markSquare(0, 1, 1)).to.equal("Player 1 wins");
+      expect(markSquare(0, 1, 1)).to.equal("Player 1 wins!!");
     });
   }); //end inner describe()
 
 
   describe('New game: Player 2 wins in middle row', function() {
     before(function() {
-      board = [[0, 0, 0], [0, 0, 0], [0, 0, 0]];
+      board = [[9, 9, 9], [9, 9, 9], [9, 9, 9]];
     });
 
     it("Player 1 marks the top left square", function() {
-      expect(markSquare(0, 0, 1)).to.eql([[1, 0, 0], [0, 0, 0], [0, 0, 0]]);
+      expect(markSquare(0, 0, 1)).to.eql([[1, 9, 9], [9, 9, 9], [9, 9, 9]]);
     });
 
     it("Player 2 marks the middle square", function() {
-      expect(markSquare(1, 1, 2)).to.eql([[1, 0, 0], [0, 2, 0], [0, 0, 0]]);
+      expect(markSquare(1, 1, 2)).to.eql([[1, 9, 9], [9, 2, 9], [9, 9, 9]]);
     });
 
     it("Player 1 marks the upper right square", function() {
-      expect(markSquare(0, 2, 1)).to.eql([[1, 0, 1], [0, 2, 0], [0, 0, 0]]);
+      expect(markSquare(0, 2, 1)).to.eql([[1, 9, 1], [9, 2, 9], [9, 9, 9]]);
     });
 
     it("Player 2 marks the middle right square", function() {
-      expect(markSquare(1, 2, 2)).to.eql([[1, 0, 1], [0, 2, 2], [0, 0, 0]]);
+      expect(markSquare(1, 2, 2)).to.eql([[1, 9, 1], [9, 2, 2], [9, 9, 9]]);
     });
 
     it("Player 1 marks the bottom right square", function() {
-      expect(markSquare(2, 2, 1)).to.eql([[1, 0, 1], [0, 2, 2], [0, 0, 1]]);
+      expect(markSquare(2, 2, 1)).to.eql([[1, 9, 1], [9, 2, 2], [9, 9, 1]]);
     });
 
     it("Player 2 marks the left middle square and wins", function() {
-      expect(markSquare(1, 0, 2)).to.equal("Player 2 wins");
+      expect(markSquare(1, 0, 2)).to.equal("Player 2 wins!!");
     });
   }); //end inner describe()
 
 
   describe('New game: Player 2 wins in bottom row', function() {
     before(function() {
-      board = [[0, 0, 0], [0, 0, 0], [0, 0, 0]];
+      board = [[9, 9, 9], [9, 9, 9], [9, 9, 9]];
     });
 
     it("Player 1 marks the top left square", function() {
-      expect(markSquare(0, 0, 1)).to.eql([[1, 0, 0], [0, 0, 0], [0, 0, 0]]);
+      expect(markSquare(0, 0, 1)).to.eql([[1, 9, 9], [9, 9, 9], [9, 9, 9]]);
     });
 
     it("Player 2 marks the bottom middle square", function() {
-      expect(markSquare(2, 1, 2)).to.eql([[1, 0, 0], [0, 0, 0], [0, 2, 0]]);
+      expect(markSquare(2, 1, 2)).to.eql([[1, 9, 9], [9, 9, 9], [9, 2, 9]]);
     });
 
     it("Player 1 marks the upper right square", function() {
-      expect(markSquare(0, 2, 1)).to.eql([[1, 0, 1], [0, 0, 0], [0, 2, 0]]);
+      expect(markSquare(0, 2, 1)).to.eql([[1, 9, 1], [9, 9, 9], [9, 2, 9]]);
     });
 
     it("Player 2 marks the bottom right square", function() {
-      expect(markSquare(2, 2, 2)).to.eql([[1, 0, 1], [0, 0, 0], [0, 2, 2]]);
+      expect(markSquare(2, 2, 2)).to.eql([[1, 9, 1], [9, 9, 9], [9, 2, 2]]);
     });
 
     it("Player 1 marks the middle right square", function() {
-      expect(markSquare(1, 2, 1)).to.eql([[1, 0, 1], [0, 0, 1], [0, 2, 2]]);
+      expect(markSquare(1, 2, 1)).to.eql([[1, 9, 1], [9, 9, 1], [9, 2, 2]]);
     });
 
     it("Player 2 marks the bottom left square and wins", function() {
-      expect(markSquare(2, 0, 2)).to.equal("Player 2 wins");
+      expect(markSquare(2, 0, 2)).to.equal("Player 2 wins!!");
     });
   }); //end inner describe()
 
 
   describe('New game: Player 2 wins in left column', function() {
     before(function() {
-      board = [[2, 0, 0], [2, 0, 0], [0, 0, 0]];
+      board = [[2, 9, 9], [2, 9, 9], [9, 9, 9]];
     });
     it("Player 2 marks the bottom left square and wins", function() {
-      expect(markSquare(2, 0, 2)).to.equal("Player 2 wins");
+      expect(markSquare(2, 0, 2)).to.equal("Player 2 wins!!");
     });
   }); //end inner describe()
 
 
   describe('New game: Player 1 wins in middle column', function() {
     before(function() {
-      board = [[0, 1, 0], [0, 1, 0], [0, 0, 0]];
+      board = [[9, 1, 9], [9, 1, 9], [9, 9, 9]];
     });
     it("Player 1 marks the bottom middle square and wins", function() {
-      expect(markSquare(2, 1, 1)).to.equal("Player 1 wins");
+      expect(markSquare(2, 1, 1)).to.equal("Player 1 wins!!");
     });
   }); //end inner describe()
 
 
   describe('New game: Player 2 wins in right column', function() {
     before(function() {
-      board = [[0, 0, 2], [0, 0, 2], [0, 0, 0]];
+      board = [[9, 9, 2], [9, 9, 2], [9, 9, 9]];
     });
     it("Player 2 marks the bottom right square and wins", function() {
-      expect(markSquare(2, 2, 2)).to.equal("Player 2 wins");
+      expect(markSquare(2, 2, 2)).to.equal("Player 2 wins!!");
     });
   }); //end inner describe()
 
 
   describe('New game: Player 1 wins diagonally down-right', function() {
     before(function() {
-      board = [[1, 0, 0], [0, 1, 0], [0, 0, 0]];
+      board = [[1, 9, 9], [9, 1, 9], [9, 9, 9]];
     });
     it("Player 1 marks the bottom right square and wins", function() {
-      expect(markSquare(2, 2, 1)).to.equal("Player 1 wins");
+      expect(markSquare(2, 2, 1)).to.equal("Player 1 wins!!");
     });
   }); //end inner describe()
 
 
   describe('New game: Player 2 wins diagonally up-right', function() {
     before(function() {
-      board = [[0, 0, 2], [0, 2, 0], [0, 0, 0]];
+      board = [[9, 9, 2], [9, 2, 9], [9, 9, 9]];
     });
     it("Player 2 marks the bottom left square and wins", function() {
-      expect(markSquare(2, 0, 2)).to.equal("Player 2 wins");
+      expect(markSquare(2, 0, 2)).to.equal("Player 2 wins!!");
     });
   }); //end inner describe()
 
