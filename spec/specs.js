6348da44973bdce4b01522fac71a8400d117f14f
Kenny <student@epicodus.com>
2015-09-16 09:32:01 -0700

    pass 1 test

--- spec/specs.js
+++ spec/specs.js
@@ -1,9 +1,15 @@
-describe('Contact', function() {
-  it("creates a new contact with the given specifications", function() {
-    var testContact = new Contact("Rita","Moreno");
-    expect(testContact.firstName).to.equal("Rita");
-    expect(testContact.lastName).to.equal("Moreno");
-    expect(testContact.addresses).to.eql([]);
+describe('markSquare', function() {
+  it("Player 1 marks the middle square", function() {
+    expect(markSquare(1, 1, 1)).to.eql([[0, 0, 0], [0, 1, 0], [0, 0, 0]]);
   });
 
 });
+
+
+
+
+// expect(markSquare(1, 1, 1)).to.equal("Space already taken");
+// var testContact = new Contact("Rita","Moreno");
+// expect(testContact.firstName).to.equal("Rita");
+// expect(testContact.lastName).to.equal("Moreno");
+// expect(testContact.addresses).to.eql([]);
