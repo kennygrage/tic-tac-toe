c18f1830c99f381f6537d3cfb0273b6a7a1cd0d6
Kenny <student@epicodus.com>
2015-09-17 16:06:24 -0700

    computer hard mode looks at 4 logic tests

--- js/scripts.js
+++ js/scripts.js
@@ -6,6 +6,7 @@ var player_turn;                               //whose turn is it?
 var player_letter = ["", "X", "O"];            //Player letter
 var number_of_players;                         //Either 2 players or 1 player with computer
 var difficulty;                                //1 = easy & 2 = hard
+var winCombos = [ [0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6] ];
 var board = [[9, 9, 9], [9, 9, 9], [9, 9, 9]]; //9 = empty space, 1 = "X", 2 = "O"
 // Board[x][y] locations:
 // [0][0] - [0][1] - [0][2]
@@ -16,6 +17,7 @@ var board = [[9, 9, 9], [9, 9, 9], [9, 9, 9]]; //9 = empty space, 1 = "X", 2 = "
 
 
 
+//**********Functions**********//
 //**********Print Player 1's Turn on the bottom and set player_turn = 1***********//
 function startGame() {
   //start with player one
@@ -73,22 +75,59 @@ function outputText(text_going_into_output) {
 
 
 
+//**********Convert the number 0-8 to the Div ID**********//
+function convertToBoard(num) {
+  if (num == 0) {
+    return "_0_0";
+  }
+  else if (num == 1) {
+    return "_0_1";
+  }
+  else if (num == 2) {
+    return "_0_2";
+  }
+  else if (num == 3) {
+    return "_1_0";
+  }
+  else if (num == 4) {
+    return "_1_1";
+  }
+  else if (num == 5) {
+    return "_1_2";
+  }
+  else if (num == 6) {
+    return "_2_0";
+  }
+  else if (num == 7) {
+    return "_2_1";
+  }
+  else if (num == 8) {
+    return "_2_2";
+  }
+} //end convertToBoard()
+
+
+
 //**********Check to see if the current player won**********//
 function win() {
-  if ((board[0][0] == board[0][1] && board[0][0] == board[0][2] && board[0][0] < 9) || //top row
-      (board[1][0] == board[1][1] && board[1][0] == board[1][2] && board[1][0] < 9) || //middle row
-      (board[2][0] == board[2][1] && board[2][0] == board[2][2] && board[2][0] < 9) || //bottom row
-      (board[0][0] == board[1][0] && board[0][0] == board[2][0] && board[0][0] < 9) || //left column
-      (board[0][1] == board[1][1] && board[0][1] == board[2][1] && board[0][1] < 9) || //middle column
-      (board[0][2] == board[1][2] && board[0][2] == board[2][2] && board[0][2] < 9) || //right column
-      (board[0][0] == board[1][1] && board[0][0] == board[2][2] && board[0][0] < 9) || //from upper left to lower right diagonal
-      (board[0][2] == board[1][1] && board[0][2] == board[2][0] && board[0][2] < 9) )  //from upper right to lower left diagonal
-      {
-    return true;
+  var win_row;
+  var win_column;
+  var win_diagonal_left = board[1][1];
+  var win_diagonal_right = board[1][1];
+    for (var i = 0; i <= 2; i++) {
+      win_row = board[i][0] + board[i][1] + board[i][2];
+      win_column = board[0][i] + board[1][i] + board[2][i];
+      if (win_row == 3 || win_row == 6 || win_column == 3 || win_column == 6) {
+        return true;
+      }
   }
-  else {
-    return false;
+  win_diagonal_left += board[0][0] + board[2][2];
+  win_diagonal_right += board[0][2] + board[2][0];
+  if (win_diagonal_left == 3 || win_diagonal_left == 6 ||
+      win_diagonal_right == 3 || win_diagonal_right == 6) {
+      return true;
   }
+  return false;
 } //end win()
 
 
@@ -180,14 +219,138 @@ function markSquareOnBoard(clicked) {
   //not part of the if..else if chain above so that player 1 doesn't need to click for player 2 computer to go
   //player 2's turn (computer) on hard mode
   if (player_turn == 2 && number_of_players == 1 && difficulty == 2) {
-    if (markSquareInArrayResult == "Player 1 wins!!" && player_turn < 9) {
-      player_turn = 1; //for the correct colors going into the proceeding functions
-      playerTurnText(markSquareInArrayResult); //Player (1 or 2) wins !! results in "Game Over" output
-      outputText(markSquareInArrayResult);
-      player_turn = 9; //make sure the game can't be played anymore
-      $("#new_game_button").show();
+    //win when 2 in a row
+    var winnerArray = [];
+    var found_a_spot = 0;
+    for (var i = 0; i<=2; i++) {
+      for (var j = 0; j<=2; j++) {
+        winnerArray.push(board[i][j]);
+      }
     }
-    if (number_of_players == 1 && checkForEmptySpaces()) {
+    for (var j = 0; j<=7; j++) {
+      var solution = winnerArray[winCombos[j][0]] + winnerArray[winCombos[j][1]] + winnerArray[winCombos[j][2]];
+      if (solution == 13) { //two places filled with "2" and the other empty filled with "9"
+        if (winnerArray[winCombos[j][0]] == 9) {
+          clicked = convertToBoard(winCombos[j][0]);
+          found_a_spot++;
+        }
+        else if (winnerArray[winCombos[j][1]] == 9) {
+          clicked = convertToBoard(winCombos[j][1]);
+          found_a_spot++;
+        }
+        else if (winnerArray[winCombos[j][2]] == 9) {
+          clicked = convertToBoard(winCombos[j][2]);
+          found_a_spot++;
+        }
+      }
+      if (found_a_spot > 0) {break;}
+    }
+
+    //block when other has 2 in a row
+    if (found_a_spot == 0) { //only look to block if we haven't found a spot to win
+      for (var j = 0; j<=7; j++) {
+        var solution = winnerArray[winCombos[j][0]] + winnerArray[winCombos[j][1]] + winnerArray[winCombos[j][2]];
+        if (solution == 11) { //player one has two in a row and there is one empty space, so block it
+          if (winnerArray[winCombos[j][0]] == 9) {
+            clicked = convertToBoard(winCombos[j][0]);
+            found_a_spot++;
+          }
+          else if (winnerArray[winCombos[j][1]] == 9) {
+            clicked = convertToBoard(winCombos[j][1]);
+            found_a_spot++;
+          }
+          else if (winnerArray[winCombos[j][2]] == 9) {
+            clicked = convertToBoard(winCombos[j][2]);
+            found_a_spot++;
+          }
+        }
+        if (found_a_spot > 0) {break;}
+      }
+    }
+
+    //create 2 in a row
+    if (found_a_spot == 0) { //only look to create 2 in a row if we cannot win or block
+      for (var j = 0; j<=7; j++) {
+        solution = winnerArray[winCombos[j][0]] + winnerArray[winCombos[j][1]] + winnerArray[winCombos[j][2]];
+        if (solution == 20) { //player two has one spot and there are two empty spaces
+          if (winnerArray[winCombos[j][0]] == 9) {
+            clicked = convertToBoard(winCombos[j][0]);
+            found_a_spot++;
+          }
+          else if (winnerArray[winCombos[j][1]] == 9) {
+            clicked = convertToBoard(winCombos[j][1]);
+            found_a_spot++;
+          }
+          else if (winnerArray[winCombos[j][2]] == 9) {
+            clicked = convertToBoard(winCombos[j][2]);
+            found_a_spot++;
+          }
+        }
+        if (found_a_spot > 0) {break;}
+      }
+    }
+
+    //if center space is not taken, take it
+    if (found_a_spot == 0) { //only do this if we haven't found a spot yet
+      if (board[1][1] == 9) {
+        clicked = convertToBoard(4); //Div ID for "_1_1"
+        found_a_spot++;
+      }
+    }
+
+
+
+
+    if (found_a_spot > 0) {
+      var x_position = clicked.substring(1,2);
+      var y_position = clicked.substring(3,4);
+      markSquareInArrayResult = markSquareInArray(x_position, y_position, player_turn);
+      placeLetter(clicked);
+      if (markSquareInArrayResult == "Player 2 wins!!") {
+        playerTurnText(markSquareInArrayResult); //Player (1 or 2) wins !! results in "Game Over" output
+        outputText(markSquareInArrayResult);
+        player_turn = 9; //make sure the game can't be played anymore
+        $("#new_game_button").show();
+      }
+      else {
+        player_turn = 1;
+        playerTurnText("Player 1's Turn");
+        outputText(""); //empty output incase an error message was there
+      }
+    }
+
+
+
+
+
+
+
+
+    //random
+
+
+
+
+
+
+
+
+
+                              // if (markSquareInArrayResult == "Player 1 wins!!" && player_turn < 9) {
+                              //   player_turn = 1; //for the correct colors going into the proceeding functions
+                              //   playerTurnText(markSquareInArrayResult); //Player (1 or 2) wins !! results in "Game Over" output
+                              //   outputText(markSquareInArrayResult);
+                              //   player_turn = 9; //make sure the game can't be played anymore
+                              //   $("#new_game_button").show();
+                              // }
+
+
+
+
+
+
+
+    if (number_of_players == 1 && checkForEmptySpaces() && found_a_spot == 0) {
       do {
         var done = 0;
         var random_x = Math.floor(Math.random() * 3); //random number between 0 and 2
@@ -204,6 +367,7 @@ function markSquareOnBoard(clicked) {
             $("#new_game_button").show();
           }
           else {
+            player_turn = 1;
             playerTurnText("Player 1's Turn");
             outputText(""); //empty output incase an error message was there
           }
@@ -276,16 +440,3 @@ $(document).ready(function() {
     }
   }); //end document.body
 }); //end document.ready
-
-
-
-
-
-// else if (markSquareInArrayResult == "Player 2 wins!!" && player_turn < 9) {
-//   $("#" + clicked).css("color", player_two_color).text("O");
-//   player_turn = 9; //make sure the game can't be played anymore
-//   $("#player_turn").css("color", player_two_color);
-//   $("#player_turn").text("Game Over");
-//   $("#output").text(markSquareInArrayResult);
-//   $("#new_game_button").show();
-// }
